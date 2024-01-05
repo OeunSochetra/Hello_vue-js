1512f57954db8e4c5ebe3ffed3bf4870503d6a02
OeunSochetra <oeunsochetra@gmail.com>
2024-01-05 23:30:58 +0700

    first time buil with only UI

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpense from "./components/IncomeExpense.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <div class="container">
+    <Balance></Balance>
+    <IncomeExpense></IncomeExpense>
+    <TransactionList></TransactionList>
+    <AddTransaction></AddTransaction>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+export interface ITransaction {
+  id: number;
+  amount: number;
+  text: string;
+}
+
+const transaction: ITransaction[] = [
+  { id: 1, text: "Flower", amount: 45.9 },
+  { id: 1, text: "Book", amount: 12.9 },
+  { id: 1, text: "Camera", amount: 89.9 },
+  { id: 1, text: "Phone", amount: 100.9 },
+];
+</script>
+
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="items in transaction"
+      v-bind:key="items.id"
+      :class="items.amount > 0 ? 'minus' : 'plus'"
+    >
+      {{ items.text }} <span>-${{ items.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+      Paycheck <span>$800</span>
+      <button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
